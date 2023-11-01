84d7866446bfd866d9342ffd743601aa3515f157
DaniyalNazeer <112878517+DaniyalNazeer@users.noreply.github.com>
2023-11-01 23:53:21 +0500

    Update index.js

--- app/components/layouts/header/index.js
+++ app/components/layouts/header/index.js
@@ -31,20 +31,14 @@ const Header = ({ data }) => {
                     <Link href="/">
 
 
-	    {
-		     <Image
-            // style={{ width:100 , height : 400 }} 
-            src={ siteLogoUrl}
-            width={'500px'}
-            height={'500px'}
-            alt="Picture of the author"
-            
-            
-            />
-	    }
+	   { siteLogoUrl ? ( <Image className="mr-2" src={ siteLogoUrl } alt={ `${ siteTitle } logo` }
+	width="100%"
+	height=""/>
+	) : <TailwindIcon/>
+	}
 					
 								
-							</Link>      
+		</Link>      
 
                             <ul className="navbar flex">
                             
